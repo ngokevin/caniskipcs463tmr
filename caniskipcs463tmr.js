f12cf8d9190f44dc2627e6a0629ada72c89ca089
Kevin Ngo <ngoke@mozilla.com>
2013-04-02 23:45:35 -0700

    initial commit of the next google

---
+++ caniskipcs463tmr.js
@@ -0,0 +1,41 @@
+var irc = require("irc");
+
+
+var config = {
+    channels: ["#caniskipcs463tmr"],
+    server: "irc.freenode.net",
+    botName: "caniskipcs463tmr"
+};
+var bot = new irc.Client(config.server, config.botName, {
+    channels: config.channels
+});
+
+
+bot.addListener("message", function(from, to, text, message) {
+    var dates = {
+        // Define day before classes (0-11 month).
+        '20130309': 'Walk-throughs',
+        '20130330': 'Speaker: Credit Scores',
+        '20130407': 'Elevator Talks',
+        '20130428': 'Speaker: Parallel Computing',
+    }
+
+    if (text.indexOf('caniskipcs463tmr') !== -1) {
+        var currentDate = calcTime(-7);  // Pacific Daylight Time.
+        var prettyDate = (currentDate.getFullYear() + ("0" + currentDate.getMonth()).substr(-2) +
+                          ("0" + currentDate.getDate()).substr(-2));
+
+        if (prettyDate in dates) {
+            bot.say(config.channels[0], "Get your ass to class tomorrow. " + dates[prettyDate]);
+        } else {
+            bot.say(config.channels[0], "Skip, ain't nobody got time for that.");
+        }
+    }
+});
+
+
+function calcTime(offset) {
+    d = new Date();
+    utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+    return new Date(utc + (3600000*offset));
+}
