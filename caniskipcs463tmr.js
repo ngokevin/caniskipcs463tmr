ce97f310b725915e9f88f4b4bf761fe652db0438
Kevin Ngo <me@ngokevin.com>
2013-04-04 05:12:27 +0000

    check for day of week, ident with nickserv, invite with chanserv

--- caniskipcs463tmr.js
+++ caniskipcs463tmr.js
@@ -1,22 +1,30 @@
-var irc = require("irc");
+var irc = require('irc');
 
 
 var config = {
-    channels: ["#caniskipcs463tmr"],
-    server: "irc.freenode.net",
-    botName: "caniskipcs463tmr"
-    password: "password"
+    channel: '#osu-cs463',
+    server: 'irc.freenode.net',
+    botName: 'caniskipcs463tmr',
+    password: 'password'
 };
-var bot = new irc.Client(config.server, config.botName, {
-    channels: config.channels
+var bot = new irc.Client(config.server, config.botName);
+
+
+bot.addListener('error', function(message) {
+    console.log(message);
 });
 
 
-bot.say('NickServ', 'identify ' + config.password);
-bot.say('ChanServ', 'invite ' + config.channels[0]);
+bot.addListener('registered', function(message) {
+    console.log('Identifying and inviting.');
+    bot.send('/msg nickserv identify ' + config.password);
+    bot.send('/msg chanserv invite ' + config.channel);
+    setTimeout(function() {
+        bot.join(config.channel);
+    }, 20000);
+});
 
-
-bot.addListener("message", function(from, to, text, message) {
+bot.addListener('message', function(from, to, text, message) {
     var dates = {
         // Define day before classes (0-11 month).
         '20130309': 'Walk-throughs',
@@ -27,13 +35,19 @@ bot.addListener("message", function(from, to, text, message) {
 
     if (text.indexOf('caniskipcs463tmr') !== -1) {
         var currentDate = calcTime(-7);  // Pacific Daylight Time.
-        var prettyDate = (currentDate.getFullYear() + ("0" + currentDate.getMonth()).substr(-2) +
-                          ("0" + currentDate.getDate()).substr(-2));
+
+        if ([1, 3].indexOf(currentDate.getDay()) === -1) {
+            bot.say(config.channel, "Son be trippin'. You don't have CS463 tomorrow.");
+            return;
+        }
+
+        var prettyDate = (currentDate.getFullYear() + ('0' + currentDate.getMonth()).substr(-2) +
+                          ('0' + currentDate.getDate()).substr(-2));
 
         if (prettyDate in dates) {
-            bot.say(config.channels[0], "Get your ass to class tomorrow. " + dates[prettyDate]);
+            bot.say(config.channel, 'Get your ass to class tomorrow. ' + dates[prettyDate]);
         } else {
-            bot.say(config.channels[0], "Skip, ain't nobody got time for that.");
+            bot.say(config.channel, "Skip, ain't nobody got time for that.");
         }
     }
 });
